8e654594854d61494d3ff6895fc4c47bfc83de05
Marty McGuire <schmartissimo@gmail.com>
2019-03-13 15:41:38 -0400

    Chunk buffer sends into 64 byte chunks
    Prevents errors with unsent data on large header values, long URL paths

--- adafruit_esp32spi/adafruit_esp32spi.py
+++ adafruit_esp32spi/adafruit_esp32spi.py
@@ -527,11 +527,15 @@ class ESP_SPIcontrol:  # pylint: disable=too-many-public-methods
         if self._debug:
             print("Writing:", buffer)
         self._socknum_ll[0][0] = socket_num
-        resp = self._send_command_get_response(_SEND_DATA_TCP_CMD,
-                                               (self._socknum_ll[0], buffer),
-                                               sent_param_len_16=True)
+        chunk_size = 64
+        sent = 0
+        for chunk in range((len(buffer) // chunk_size)+1):
+            chunk_buffer = buffer[(chunk*chunk_size):((chunk+1)*chunk_size)]
+            resp = self._send_command_get_response(_SEND_DATA_TCP_CMD,
+                                                   (self._socknum_ll[0], chunk_buffer),
+                                                   sent_param_len_16=True)
+            sent += resp[0][0]
 
-        sent = resp[0][0]
         if sent != len(buffer):
             raise RuntimeError("Failed to send %d bytes (sent %d)" % (len(buffer), sent))
 
