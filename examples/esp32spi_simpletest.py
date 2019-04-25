848777af3e6c7f4d1e62f5aa50b662007f52549b
brentru <robots199@me.com>
2019-04-25 12:46:02 -0400

    default to predefined pins, make external pin def optional

--- examples/esp32spi_simpletest.py
+++ examples/esp32spi_simpletest.py
@@ -11,16 +11,16 @@ TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
 JSON_URL = "http://api.coindesk.com/v1/bpi/currentprice/USD.json"
 
 
-# If you have an externally connected ESP32:
-esp32_cs = DigitalInOut(board.D9)
-esp32_ready = DigitalInOut(board.D10)
-esp32_reset = DigitalInOut(board.D5)
-
 # If you are using a board with pre-defined ESP32 Pins:
-"""
 esp32_cs = DigitalInOut(board.ESP_CS)
 esp32_ready = DigitalInOut(board.ESP_BUSY)
 esp32_reset = DigitalInOut(board.ESP_RESET)
+
+# If you have an externally connected ESP32:
+"""
+esp32_cs = DigitalInOut(board.D9)
+esp32_ready = DigitalInOut(board.D10)
+esp32_reset = DigitalInOut(board.D5)
 """
 
 spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
