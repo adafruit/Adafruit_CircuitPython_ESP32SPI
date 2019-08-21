4ee72456b658358e1461acd459e14b170d62fbe5
mytechnotalent <kevin@mytechnotalent.com>
2019-08-21 06:49:22 -0400

    Fixed MAC reverse byte order issue within the ESP32SPI library as it was originally echoing the MAC address in reverse byte order.  Added portable MAC function call within WSGISERVER.PY example to properly echo MAC address when running example.

--- examples/server/esp32spi_wsgiserver.py
+++ examples/server/esp32spi_wsgiserver.py
@@ -8,9 +8,6 @@ from adafruit_esp32spi import adafruit_esp32spi
 import adafruit_esp32spi.adafruit_esp32spi_wifimanager as wifimanager
 import adafruit_esp32spi.adafruit_esp32spi_wsgiserver as server
 
-
-
-
 # This example depends on the 'static' folder in the examples folder
 # being copied to the root of the circuitpython filesystem.
 # This is where our static assets like html, js, and css live.
