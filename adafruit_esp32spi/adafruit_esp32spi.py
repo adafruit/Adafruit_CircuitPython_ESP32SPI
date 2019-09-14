446beca9abda21a68c4f9dc36690bacc1c4c7c38
Jerry Needell <jerryneedell@gmail.com>
2019-09-13 22:18:49 -0400

    raise error in get_time if zero returned

--- adafruit_esp32spi/adafruit_esp32spi.py
+++ adafruit_esp32spi/adafruit_esp32spi.py
@@ -766,6 +766,8 @@ class ESP_SPIcontrol:  # pylint: disable=too-many-public-methods
         """The current unix timestamp"""
         if self.status == WL_CONNECTED:
             resp = self._send_command_get_response(_GET_TIME)
+            if struct.unpack('<i', resp[0]) == (0,):
+                raise ValueError("_GET_TIME returned 0")
             return struct.unpack('<i', resp[0])
         if self.status in (WL_AP_LISTENING, WL_AP_CONNECTED):
             raise RuntimeError("Cannot obtain NTP while in AP mode, must be connected to internet")
