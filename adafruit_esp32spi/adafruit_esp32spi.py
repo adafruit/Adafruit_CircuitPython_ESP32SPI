32f15cedf0d0faed82379ba6b832e75ae9643736
Jerry Needell <jerryneedell@gmail.com>
2019-09-14 22:16:05 -0400

    simplify get_time valid tst

--- adafruit_esp32spi/adafruit_esp32spi.py
+++ adafruit_esp32spi/adafruit_esp32spi.py
@@ -766,9 +766,10 @@ class ESP_SPIcontrol:  # pylint: disable=too-many-public-methods
         """The current unix timestamp"""
         if self.status == WL_CONNECTED:
             resp = self._send_command_get_response(_GET_TIME)
-            if struct.unpack('<i', resp[0]) == (0,):
+            resp_time = struct.unpack('<i', resp[0])
+            if resp_time == (0,):
                 raise ValueError("_GET_TIME returned 0")
-            return struct.unpack('<i', resp[0])
+            return resp_time
         if self.status in (WL_AP_LISTENING, WL_AP_CONNECTED):
             raise RuntimeError("Cannot obtain NTP while in AP mode, must be connected to internet")
         raise RuntimeError("Must be connected to WiFi before obtaining NTP.")
