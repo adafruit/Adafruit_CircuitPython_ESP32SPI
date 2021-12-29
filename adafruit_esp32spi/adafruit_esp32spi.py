b037a5374b8239f0f296464b5926a2073d45fcba
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-12-29 12:47:11 -0500

    Change cs_pin reference to cs_dio

--- adafruit_esp32spi/adafruit_esp32spi.py
+++ adafruit_esp32spi/adafruit_esp32spi.py
@@ -144,7 +144,7 @@ class ESP_SPIcontrol:  # pylint: disable=too-many-public-methods, too-many-insta
         self._sendbuf = bytearray(256)  # buffer for command sending
         self._socknum_ll = [[0]]  # pre-made list of list of socket #
 
-        self._spi_device = SPIDevice(spi, cs_pin, baudrate=8000000)
+        self._spi_device = SPIDevice(spi, cs_dio, baudrate=8000000)
         self._cs = cs_dio
         self._ready = ready_dio
         self._reset = reset_dio
