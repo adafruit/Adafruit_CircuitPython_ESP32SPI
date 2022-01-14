2bc4d13aaa65695ff431d1158d1dc74401854f4f
Alec Delaney <tekktrik@gmail.com>
2022-01-14 11:48:29 -0500

    Minor formatting fixes

--- adafruit_esp32spi/adafruit_esp32spi.py
+++ adafruit_esp32spi/adafruit_esp32spi.py
@@ -21,7 +21,8 @@ Implementation Notes
 * Adafruit CircuitPython firmware for the supported boards:
   https://github.com/adafruit/circuitpython/releases
 
-* Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+* Adafruit's Bus Device library:
+  https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
 
 """
 
